cf5b09ffc4b6ac65dd8593db3cfd0d938713a4dc
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-22 15:21:30 +0300

    Update _header.php

--- _header.php
+++ _header.php
@@ -1,3 +1,17 @@
-<?php
+<?php if (file_exists(__DIR__ . '/css/bootstrap.min.css')): ?>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+<?php else: ?>
+    <?php die('Something went wrong! Please check your directory and try again.'); ?>
+<?php endif; ?>
 
-?>
+<?php if (file_exists(__DIR__ . '/css/custom.css')): ?>
+    <link rel="stylesheet" href="/css/custom.css">
+<?php else: ?>
+    <?php die('Something went wrong! Please check your directory and try again.'); ?>
+<?php endif; ?>
+
+<?php if (file_exists(__DIR__ . '/js/jquery-3.4.1.min.js')): ?>
+    <script src="/js/jquery-3.4.1.min.js"></script>
+<?php else: ?>
+    <?php die('Something went wrong! Please check your directory and try again.'); ?>
+<?php endif; ?>
