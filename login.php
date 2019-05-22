e7d669aef69c4440556ec5c1afb1c8f6b4fa4c5b
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-22 15:27:23 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,29 +2,20 @@
 
 $startTime = microtime(1);
 $startMem  = memory_get_usage();
+session_start();
+$length = 32;
+$_SESSION['token'] = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $length);
+$_SESSION['token-expire'] = time() + 3600;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Software Security Shop</title>
-    <?php if (file_exists(__DIR__ . '/css/bootstrap.min.css')): ?>
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <?php else: ?>
-        <?php die('Something went wrong! Please check your directory and try again.'); ?>
-    <?php endif; ?>
-    
-    <?php if (file_exists(__DIR__ . '/js/jquery-3.4.1.min.js')): ?>
-        <script src="/js/jquery-3.4.1.min.js"></script>
-    <?php else: ?>
-        <?php die('Something went wrong! Please check your directory and try again.'); ?>
-    <?php endif; ?>
+  <?php if (file_exists(__DIR__ . '/_header.php')): ?>
+    <?php include_once(__DIR__ . '/_header.php'); ?>
+  <?php endif; ?>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <h1>Information Security Eshop</h1>
-        </div>
-    </div>
 
 <?php
 if (file_exists(__DIR__ . '/_header.php'))
@@ -37,33 +28,30 @@ else if (!file_exists(__DIR__ . '/_header.php'))
 	die('Something went wrong! Please check your directory and try again.');
 }
 ?>
-<div class="container">
-    <div class="row">
-        <h3>Test Log in</h3>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <form>
+
+    <nav class="navtop">
+		<div>
+			<h1>Software Security Shop</h1>
+			<a href="login.php"><i class="fas fa-sign-in-alt"></i>Login</a>
+		</div>
+	</nav>
+    <div class="content">
+        <div class="row">
+            <form method="post" action="validate.php">
             <div class="form-group">
-              <label for="user_username">Username</label>
-              <input type="text" class="form-control" id="user_username" placeholder="Enter username">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" name="username" id="username" placeholder="Enter username">
             </div>
             <div class="form-group">
-              <label for="user_password">Password</label>
-              <input type="password" class="form-control" id="user_password">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+              <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>"/>
             </div>
-            <button type="submit" class="btn btn-primary">Log in</button>
+            <button type="submit" class="btn btn-primary">Log in</button><br>
             <span><i>In case you forgot your password, please contact the system administrator at <a href="mailto:info@administrator.example">info@administrator.example</a></i></span>
             </form>
         </div>
-        <div class="col-lg-12 col-md-12">
-        </div>
     </div>
-</div>
-
-
 
 <?php
 if (file_exists(__DIR__ . '/_footer.php'))
