67025fff7c07042d64d14d444449da8b391686be
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-23 11:57:39 +0300

    Update addtocart.php

--- addtocart.php
+++ addtocart.php
@@ -1,20 +1,29 @@
 <?php
 session_start();
 
+/*
+ * Token validation from $.ajax request
+ */
+
 if($_SESSION['token'] != $_POST['token']){
     header('Location: login.php');
 	exit();
 }
 else{
-
+    
+    /*
+     * User session validation (TRUE || FALSE)
+     */
+	
     if (!isset($_SESSION['useron'])) {
     	header('Location: login.php');
     	exit();
     }
-    /*
-     * SKUs = 'LPN45', 'LPX230U', 'MBP2019U', 'HPP12U'
-     */
      
+    /*
+     * Token expiration time validation
+     */
+	
     if(time() >= $_SESSION['token-expire']){
         header('Location: login.php');
     	exit();    
@@ -23,6 +32,10 @@ else{
         
         $db_handle = new SecureDB;
         
+	/*
+         * $.ajax results from Search Form
+         */
+	    
         if($_POST["searchbar"]){
           if($_POST["searchbar"] != null && $_POST["item"] == null){
             $sku = $_POST["searchbar"];
@@ -32,12 +45,15 @@ else{
           header('Content-Type: application/json');
           echo json_encode($productByCode);
         }
+	    
+	/*
+         * $.ajax results from All Products button
+         */
+	    
         if($_POST["prodQuantity"] != null && $_POST["prodPrice"] != null){
           $prodQuantity = (int)$_POST["prodQuantity"];
           $prodPrice = (float)$_POST["prodPrice"];
           $prodName = $_POST["prodName"];
-          //echo $prodQuantity;
-          //echo $prodPrice;
           $itemToCart = array(
             "prodQuantity" => $prodQuantity,
             "prodPrice"    => $prodPrice,
