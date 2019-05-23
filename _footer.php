dc8a2f06d1cb38cbc6b9caac64e1e036da6b486e
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-23 11:35:11 +0300

    Update _footer.php

--- _footer.php
+++ _footer.php
@@ -1,5 +1,14 @@
-<?php ?>
+<?php
 
+?>
 <div class="footer">
-  <p>Software Security Shop <i class="fa fa-copyright"></i> 2019</p>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-footer-div">
+            <p><a href="#cartinfo"><button class="btn btn-secondary cart-footer"><img src="/assets/images/cart.png" width="30" height="30" /> <span id="cartbadge">0</span></button></a></p>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <p class="copyright">Software Security Shop <i class="fa fa-copyright"></i> 2019</p>          
+        </div>
+    </div>
+  
 </div>
