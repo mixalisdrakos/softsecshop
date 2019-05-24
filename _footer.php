9b43dea9958b6c271ce0aaed3cb834407617b0ef
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-24 17:09:51 +0300

    Update _footer.php

--- _footer.php
+++ _footer.php
@@ -7,9 +7,15 @@
 ?>
 <div class="footer">
     <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-footer-div">
-            <p><a href="#cartinfo"><button class="btn btn-secondary cart-footer"><img src="/assets/images/cart.png" width="30" height="30" /> <span id="cartbadge">0</span></button></a></p>
-        </div>
+        <?php if($_SERVER['REQUEST_URI'] != "/login.php"): ?>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-footer-div">
+                <p><a href="#cartinfo"><button class="btn btn-secondary cart-footer"><img src="/assets/images/cart.png" width="30" height="30" /> <span id="cartbadge">0</span></button></a></p>
+            </div>
+        <?php else: ?>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-footer-div">
+                <p>&nbsp;</p>
+            </div>
+        <?php endif; ?>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <p class="copyright">Software Security Shop <i class="fa fa-copyright"></i> 2019</p>          
         </div>
